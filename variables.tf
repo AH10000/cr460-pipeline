db4c6af74e516472c09d4198ee9cf7d1dcf97b2b
ah10000 <housmessi@live.fr>
2025-10-23 17:02:11 -0400

    fix: proper multiline variable blocks

--- variables.tf
+++ variables.tf
@@ -1,2 +1,9 @@
-variable "prefix"   { type = string, default = "cr460" }
-variable "location" { type = string, default = "canadacentral" }
+variable "prefix" {
+  type    = string
+  default = "cr460"
+}
+
+variable "location" {
+  type    = string
+  default = "canadacentral"
+}
